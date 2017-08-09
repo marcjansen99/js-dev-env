f25cc7eb20a4820d341387f613ebcf7909493623
marcjansen99 <marc.jansen@gmx.net>
2017-08-09 12:43:18 +0200

    bundling (css minification not working)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -14,9 +16,13 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    // generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // hash files using md5 so that their names change when the content changes
+    new WebpackMd5Hash(),
     // creates a separate bundle of vendor libraries so that they're cached separately
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
@@ -44,7 +50,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }

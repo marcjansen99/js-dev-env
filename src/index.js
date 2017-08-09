9f9a4fdc1f4c872e9d75bd973aa9bbf8a795cb7e
marcjansen99 <marc.jansen@gmx.net>
2017-08-09 09:13:19 +0200

    http calls

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {getUsers} from './api/userApi'
+import {deleteUser} from './api/userApi';
 
 getUsers().then(result => {
   let userBody = "";
@@ -14,4 +15,16 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = userBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
